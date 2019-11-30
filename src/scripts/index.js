51f80a0253b156be53ec352c1c203e148ccb1f67
michnowak2003 <mich.nowak2003@gmail.com>
2019-11-30 15:50:56 +0100

    navbar with hamburger menu v0.1

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,3 +1,11 @@
 import '../styles/main.scss';
 
+const navbar = document.getElementById('navbar');
+const sidebar = document.getElementById('sidebar');
+
+navbar.addEventListener('click', () => {
+    navbar.classList.toggle('navbar--active');
+    sidebar.classList.toggle('sidebar--active');
+});
+
 console.log('webpack starterkit');
